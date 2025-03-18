7183e38cb9bdbbb7265c9ac95b59c8da16bda9cd
Caio <caio-gama@outlook.com>
2025-03-18 16:21:14 -0300

    Adicionando meu arquivo Java

---
+++ src/exemplo01/PedidoLojaFisica.java
@@ -0,0 +1,11 @@
+package exemplo01;
+
+public class PedidoLojaFisica extends Pedido {
+	@Override
+	protected PedidoItem
+
+	criarItem(Produto produto, int quantidade) {
+		return new ItemPedidoLojaFisica(produto,quantidade);
+	}
+
+}
